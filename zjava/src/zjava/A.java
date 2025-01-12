97a1b2ddd32d42e762d640e6532db42ee45de31a
ramnath98 <dongareramnath55@gmail.com>
2025-01-11 23:34:23 -0400

    Update A.java

--- zjava/src/zjava/A.java
+++ zjava/src/zjava/A.java
@@ -5,6 +5,9 @@ public class A {
 	String name = "Ramnath Dongare";
 	long mob = 9881649658l;
 	String add = "Karvenagar";
+	String cname = "Universal College";
+	String uname = "Pune";
+	
 	
 	
 	
@@ -14,6 +17,9 @@ public class A {
 	System.out.println(a.name);
 	System.out.println(a.mob);
 	System.out.println(a.add);
+	System.out.println(a.cname);
+	System.out.println(a.uname);
+		
 		
 		
 	}

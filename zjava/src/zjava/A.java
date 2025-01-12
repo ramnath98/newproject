52182b06fdbb41b80e1162915da0ed2445aca3e4
ramnath98 <dongareramnath55@gmail.com>
2025-01-12 09:02:05 -0400

    add by ramnath

---
+++ zjava/src/zjava/A.java
@@ -0,0 +1,23 @@
+package zjava;
+
+public class A {
+	
+	String name = "Ramnath Dongare";
+	long mob = 9881649658l;
+	String add = "Karvenagar";
+	
+	
+	
+	public static void main(String[] args) {
+		
+		A  a = new A();
+	System.out.println(a.name);
+	System.out.println(a.mob);
+	System.out.println(a.add);
+		
+		
+	}
+	
+	
+
+}
